9232926d96d96905521d87fdf618f94401d680ab
Damola Oladipo <thedamolaoladipo@gmail.com>
2025-07-09 20:12:57 +0100

    feat: console triage for DB

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 import colors from "colors";
 
-// connecting to Database
+// Connecting to Database
 const connectDB = async (): Promise<void> => {
   try {
     const connect = await mongoose.connect(process.env.MONGODB_URL as string);
-    console.log(colors.green.bold.underline("MongoDB connected!"));
-  } catch (error) {
-    console.log(colors.red.bold.underline("Error! Connection failed"));
+    console.log(colors.green.bold.underline("✅ MongoDB connected!"));
+  } catch (error: any) {
+    console.log(colors.red.bold.underline(" MongoDB connection failed"));
+    console.error(colors.red(error.message)); 
   }
 };
 
-export default connectDB
-
+export default connectDB;
